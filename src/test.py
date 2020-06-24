6b328def736a35577b756f5a385544e98920c6c7
Kartik14 <kartikkhandelwal1998@gmail.com>
2020-06-24 09:46:47 +0530

    restructured code

--- src/test.py
+++
@@ -1,8 +0,0 @@
-from utils import normalize_string
-import json 
-
-with open('labels_char.json') as fd:
-    labels = json.load(fd)
-
-print(labels)
-print(normalize_string('call me at 8:00 pm!', labels[1:]))
